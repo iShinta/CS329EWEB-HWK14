63353e6b32fe2ba89e692d857f2353ec68e1a200
iShinta <ho.minhtri.net@gmail.com>
2016-11-18 22:25:49 -0600

    v0.1

--- index.php
+++ index.php
@@ -4,61 +4,67 @@
   function start(){
     //Selon l'avancee du quiz, on va inclure une version differente
     if(!isset($_COOKIE["id"])){ //Not in a session
+      echo "Not in a session";
       if($_SERVER['REQUEST_METHOD'] === 'POST'){ //POST
         //Check credentials
         if(isset($_POST["username"])){
           $username = $_POST["username"];
           $password = $_POST["password"];
 
-          //Get list of usernames and passwords
-          $fh = fopen("passwd", "r");
+          //Get list of users who finished the test
+          $fh = fopen("results", "r");
           //Check if username is already taken
-          $userlist = Array();
+          $usertest = Array();
           while(!feof($fh)){
             //Read Line
             $line = fgets($fh);
             $line_pieces = explode(":", $line);
-            $userlist[$line_pieces[0]] = $line_pieces[1];
+            $usertest[$line_pieces[0]] = $line_pieces[1];
           }
           fclose($fh);
 
-          //Check if name is authorized
-          if(array_key_exists($username, $userlist) && strcmp($userlist[$username], $password)){
-            //echo "Login Succeeded. Welcome ".$username. ".<br />";
-            setcookie("id", $username, time()+120);
-            setcookie("timeloggedin", time(), time()+120);
-            showLogged();
+          if(array_key_exists($username, $usertest)){ //Already took the test
+            echo "Sorry. That person already took the test";
           }else{
-            echo "Login Failed.<br />Bad username or password";
-            echo "<br />You entered username: ".$username;
-            echo "<br />and Password: ".$password;
-            echo "<br /><a href=\"index.php\"> Back to the homepage </a>";
-          }
+            //Get list of usernames and passwords
+            $fh = fopen("passwd", "r");
+            //Check if username is already taken
+            $userlist = Array();
+            while(!feof($fh)){
+              //Read Line
+              $line = fgets($fh);
+              $line_pieces = explode(":", $line);
+              $userlist[$line_pieces[0]] = $line_pieces[1];
+            }
+            fclose($fh);
 
-          //If ok
-          session_start();
+            //Check if name is authorized
+            if(array_key_exists($username, $userlist) && strcmp($userlist[$username], $password)){
+              //echo "Login Succeeded. Welcome ".$username. ".<br />";
+              setcookie("id", $username, time()+900);
+              setcookie("timeloggedin", time(), time()+900);
+              session_start();
+              ?><a href="index.php">Start the Quiz</a><?php
+            }else{
+              echo "Login Failed.<br />Bad username or password";
+              echo "<br />You entered username: ".$username;
+              echo "<br />and Password: ".$password;
+              echo "<br /><a href=\"index.php\"> Back to the homepage </a>";
+            }
+          }
         }
       }else{ //GET
         showLogin();
       }
     }else{ //In a session
-      //Check questions via Session Variable
-    }
+      echo "You're logged in";
+      //Check if time is up
 
-    if($etape == 0){ //log in
-      include 'login.php';
-    }else if($etape == 1){ //question1
-      include 'q1.php';
-    }else if($etape == 2){ //question2
-      include 'q2.php';
-    }else if($etape == 3){ //question3
-      include 'q3.php';
-    }else if($etape == 4){ //question4
-      include 'q4.php';
-    }else if($etape == 5){ //question5
-      include 'q5.php';
-    }else if($etape == 6){ //question6
-      include 'q6.php';
+
+      //If time is up, destroy session
+      //Check questions via Session Variable
+      //If last question, destroy session
+      destroySession();
     }
   }
 
@@ -72,6 +78,11 @@
     </form>
   <?php }
 
+  function destroySession(){
+    //Save state in results
+
+    //Destroy session
+  }
 ?>
 
 <html>
